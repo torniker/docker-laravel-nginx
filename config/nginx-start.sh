753f46d5946736b703e85942cc5c58809c5449b6
koko <torniker@gmail.com>
2015-05-12 16:00:41 +0400

    fpm port 9000 fixed

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -4,5 +4,5 @@ if [ "${FPM_PORT_9000_TCP_ADDR}" = "" ]; then
 	echo "Env variable FPM_PORT_9000_TCP_ADDR is missing ..."
 	exit 1
 fi
-sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+sed -i "s/%fpm-ip%/${FPM_PORT_9000_TCP_ADDR}/" /etc/nginx/nginx.conf
 exec /usr/sbin/nginx
